56da09a3050cc86be414539d13ef7e4bbf976acc
Alexandre Croteau-Pothier <alex@motioneleven.com>
2013-04-20 17:58:40 -0400

    install datatable

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
 //= require jquery-1.9.1.min
 //= require bootstrap.min
 //= require html5shiv
-//= require jquery-ui-1.10.0.custom.min.js
-//= require jquery.dropkick-1.0.0.js
-//= require jquery.tagsinput.js
+//= require jquery.dataTables
+//= require DT_bootstrap
